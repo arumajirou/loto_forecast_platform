39ac10961b7de75c7f2c4a1981b7e26b7da7d1e1
arumajirou <97777832+arumajirou@users.noreply.github.com>
2026-08-04 00:42:19 +0900

    feat(probabilistic): export multinomial DGLM model

--- src/loto/probabilistic/models/__init__.py
+++ src/loto/probabilistic/models/__init__.py
@@ -1,5 +1,6 @@
 """Probabilistic model implementations and legacy reference helpers."""
 
+from loto.probabilistic.models.dglm_native import MultinomialDGLMState, fit_multinomial_dglm
 from loto.probabilistic.models.reference import ReferencePosterior, fit_reference, posterior_draws
 from loto.probabilistic.models.subset_native import (
     ConditionalBernoulliPosterior,
@@ -8,8 +9,10 @@ from loto.probabilistic.models.subset_native import (
 
 __all__ = [
     "ConditionalBernoulliPosterior",
+    "MultinomialDGLMState",
     "ReferencePosterior",
     "fit_conditional_bernoulli_map",
+    "fit_multinomial_dglm",
     "fit_reference",
     "posterior_draws",
 ]
