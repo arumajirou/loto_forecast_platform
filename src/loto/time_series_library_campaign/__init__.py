c6cde20ab4e3c01def32a0a223171b3fcdb0a087
arumajirou <97777832+arumajirou@users.noreply.github.com>
2026-08-05 15:24:48 +0900

    [skip ci] feat(tslib): expose provider contract

---
+++ src/loto/time_series_library_campaign/__init__.py
@@ -0,0 +1,29 @@
+from .provider import (
+    GameGeometry,
+    Operation,
+    ProviderRequest,
+    ProviderResponse,
+    ProviderStatus,
+    SplitContract,
+    discover_models,
+    execute_request,
+    execute_request_file,
+    materialize_training_bundle,
+    validate_frame,
+    verify_prediction_files,
+)
+
+__all__ = [
+    "GameGeometry",
+    "Operation",
+    "ProviderRequest",
+    "ProviderResponse",
+    "ProviderStatus",
+    "SplitContract",
+    "discover_models",
+    "execute_request",
+    "execute_request_file",
+    "materialize_training_bundle",
+    "validate_frame",
+    "verify_prediction_files",
+]
