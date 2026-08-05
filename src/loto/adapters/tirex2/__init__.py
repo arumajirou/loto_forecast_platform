451fbb791b3a6b82ebfb1d92e2ab9621f5c543d6
arumajirou <97777832+arumajirou@users.noreply.github.com>
2026-08-06 08:16:20 +0900

    feat(tirex2): expose contract v2 types

---
+++ src/loto/adapters/tirex2/__init__.py
@@ -0,0 +1,23 @@
+from loto.adapters.tirex2.contracts import (
+    ArtifactReference,
+    CovariateBlock,
+    GameGeometry,
+    GpuEvidence,
+    ModelIdentity,
+    RuntimeEvidence,
+    SeriesLayout,
+    Tirex2Request,
+    Tirex2Response,
+)
+
+__all__ = [
+    "ArtifactReference",
+    "CovariateBlock",
+    "GameGeometry",
+    "GpuEvidence",
+    "ModelIdentity",
+    "RuntimeEvidence",
+    "SeriesLayout",
+    "Tirex2Request",
+    "Tirex2Response",
+]
